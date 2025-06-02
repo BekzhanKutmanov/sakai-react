5058bdafaa0c60d052482017956b82e001cc9c46
BekzhanKutmanov <kutmanov@gmail.com>
2025-06-02 17:20:43 +0600

    login page update

--- app/(full-page)/auth/login/page.tsx
+++ app/(full-page)/auth/login/page.tsx
@@ -8,6 +8,7 @@ import { Password } from 'primereact/password';
 import { LayoutContext } from '../../../../layout/context/layoutcontext';
 import { InputText } from 'primereact/inputtext';
 import { classNames } from 'primereact/utils';
+import InfoBanner from '@/app/components/InfoBanner';
 
 const LoginPage = () => {
     const [password, setPassword] = useState('');
@@ -15,38 +16,29 @@ const LoginPage = () => {
     const { layoutConfig } = useContext(LayoutContext);
 
     const router = useRouter();
-    const containerClassName = classNames('surface-ground flex align-items-center justify-content-center min-h-screen min-w-screen overflow-hidden', { 'p-input-filled': layoutConfig.inputStyle === 'filled' });
+    // const containerClassName = classNames('surface-ground flex align-items-center justify-content-center min-h-screen min-w-screen overflow-hidden', { 'p-input-filled': layoutConfig.inputStyle === 'filled' });
 
     return (
-        <div className={containerClassName}>
-            <div className="flex flex-column align-items-center justify-content-center">
-                <img src={`/layout/images/logo-${layoutConfig.colorScheme === 'light' ? 'dark' : 'white'}.svg`} alt="Sakai logo" className="mb-5 w-6rem flex-shrink-0" />
-                <div
-                    style={{
-                        borderRadius: '56px',
-                        padding: '0.3rem',
-                        background: 'linear-gradient(180deg, var(--primary-color) 10%, rgba(33, 150, 243, 0) 30%)'
-                    }}
-                >
-                    <div className="w-full surface-card py-8 px-5 sm:px-8" style={{ borderRadius: '53px' }}>
-                        <div className="text-center mb-5">
-                            <img src="/demo/images/login/avatar.png" alt="Image" height="50" className="mb-3" />
-                            <div className="text-900 text-3xl font-medium mb-3">Welcome, Isabel!</div>
-                            <span className="text-600 font-medium">Sign in to continue</span>
-                        </div>
-
+        <div className={'flex flex-col gap-4'}>
+            <InfoBanner title='Кирүү'/>
+            <div className="flex gap-4 flex-column lg:flex-row items-center justify-evenly px-4 mb-8">
+                    <div className='user-img'>
+                        <img src="/layout/images/man.png" className='w-[500px]' alt="" />
+                    </div>
+                    
+                    <div className="lg:w-1/3 shadow-2xl py-6 px-3 md:py-8 md:px-4 sm:px-8 rounded">
                         <div>
-                            <label htmlFor="email1" className="block text-900 text-xl font-medium mb-2">
-                                Email
+                            <label htmlFor="email1" className="block text-900 text-[16px] md:text-xl font-medium mb-1 md:mb-2">
+                                MyEdu email
                             </label>
-                            <InputText id="email1" type="text" placeholder="Email address" className="w-full md:w-30rem mb-5" style={{ padding: '1rem' }} />
+                            <InputText id="email1" type="text" placeholder="email@oshsu.kg" className="w-[90%] mb-5" style={{ padding: '1rem' }} />
 
-                            <label htmlFor="password1" className="block text-900 font-medium text-xl mb-2">
-                                Password
+                            <label htmlFor="password1" className="block text-900 font-medium text-[16px] md:text-xl mb-1 md:mb-2">
+                                Сыр сөз
                             </label>
-                            <Password inputId="password1" value={password} onChange={(e) => setPassword(e.target.value)} placeholder="Password" toggleMask className="w-full mb-5" inputClassName="w-full p-3 md:w-30rem"></Password>
+                            <Password inputStyle={{  }} value={password} onChange={(e) => setPassword(e.target.value)} placeholder="" toggleMask className="w-[100%] mb-5" inputClassName="w-[90%]" feedback={false}></Password>
 
-                            <div className="flex align-items-center justify-content-between mb-5 gap-5">
+                            {/* <div className="flex align-items-center justify-content-between mb-5 gap-5">
                                 <div className="flex align-items-center">
                                     <Checkbox inputId="rememberme1" checked={checked} onChange={(e) => setChecked(e.checked ?? false)} className="mr-2"></Checkbox>
                                     <label htmlFor="rememberme1">Remember me</label>
@@ -54,12 +46,11 @@ const LoginPage = () => {
                                 <a className="font-medium no-underline ml-2 text-right cursor-pointer" style={{ color: 'var(--primary-color)' }}>
                                     Forgot password?
                                 </a>
-                            </div>
-                            <Button label="Sign In" className="w-full p-3 text-xl" onClick={() => router.push('/')}></Button>
+                            </div> */}
+                            <Button label="Кирүү" className="w-full p-2 md:p-3 text-[14px] md:text-xl" onClick={() => router.push('/')}></Button>
                         </div>
                     </div>
                 </div>
-            </div>
         </div>
     );
 };
