a62cf3c29a402a64b7bd68b5be5624ed916c2cc8
BekzhanKutmanov <kutmanov@gmail.com>
2025-06-05 16:44:51 +0600

    castom profile

--- layout/AppTopbar.tsx
+++ layout/AppTopbar.tsx
@@ -1,7 +1,7 @@
 /* eslint-disable @next/next/no-img-element */
 
 import Link from 'next/link';
-import React, { forwardRef, useContext, useEffect, useImperativeHandle, useRef } from 'react';
+import React, { forwardRef, useContext, useImperativeHandle, useRef } from 'react';
 import Tiered from '@/app/components/popUp/Tiered';
 import FancyLinkBtn from '@/app/components/buttons/FancyLinkBtn';
 import { classNames } from 'primereact/utils';
@@ -9,18 +9,10 @@ import { AppTopbarRef } from '@/types';
 import { LayoutContext } from './context/layoutcontext';
 import { useMediaQuery } from '@/hooks/useMediaQuery';
 import { usePathname } from 'next/navigation';
-import { TieredMenu } from 'primereact/tieredmenu';
 import { logout } from '@/utils/logout';
 
 const AppTopbar = forwardRef<AppTopbarRef>((props, ref) => {
-    const { layoutConfig, 
-        layoutState, 
-        onMenuToggle, 
-        showProfileSidebar, 
-        user, 
-        setUser,
-        setGlobalLoading
-    } = useContext(LayoutContext);
+    const { layoutConfig, layoutState, onMenuToggle, showProfileSidebar, user, setUser, setGlobalLoading } = useContext(LayoutContext);
 
     const menubuttonRef = useRef(null);
     const topbarmenuRef = useRef(null);
@@ -66,17 +58,22 @@ const AppTopbar = forwardRef<AppTopbarRef>((props, ref) => {
                   icon: 'pi pi-user',
                   items: [
                       {
-                          label: 'Менин профилим',
-                          icon: 'pi pi-user',
-                          items: [],
-                          link: '#'
+                          label: (
+                              <div className="flex justify-center m-auto items-center flex-col gap-1 text-sm">
+                                  <div className="flex gap-1">
+                                      <span className="text-[var(--titleColor)]">{user?.last_name}</span>
+                                      <span className="text-[var(--titleColor)]">{user?.name}</span>
+                                  </div>
+                                  <span className="text-gray-500 text-[12px]">{user?.email}</span>
+                              </div>
+                          ),
                       },
                       {
                           label: 'Чыгуу',
                           icon: 'pi pi-sign-out',
                           items: [],
                           command: () => {
-                            logout({setUser, setGlobalLoading});
+                              logout({ setUser, setGlobalLoading });
                           }
                       }
                   ]
@@ -166,17 +163,22 @@ const AppTopbar = forwardRef<AppTopbarRef>((props, ref) => {
     // profile
     const profileItems = [
         {
-            label: 'Менин профилим',
-            icon: 'pi pi-user',
-            items: [],
-            link: '#'
+            label: (
+                <div className="flex items-center flex-col gap-1 text-sm">
+                    <div className="flex gap-1">
+                        <span className="text-[var(--titleColor)]">{user?.last_name}</span>
+                        <span className="text-[var(--titleColor)]">{user?.name}</span>
+                    </div>
+                    <span className="text-gray-500 text-[12px]">{user?.email}</span>
+                </div>
+            )
         },
         {
             label: 'Чыгуу',
             icon: 'pi pi-sign-out',
             items: [],
             command: () => {
-                logout({setUser, setGlobalLoading});
+                logout({ setUser, setGlobalLoading });
             }
         }
     ];
@@ -185,7 +187,7 @@ const AppTopbar = forwardRef<AppTopbarRef>((props, ref) => {
         <div className="layout-topbar">
             <Link href="/" className="layout-topbar-logo">
                 {/* <img src={`/layout/images/logo-${layoutConfig.colorScheme !== 'light' ? 'white' : 'dark'}.svg`} width="47.22px" height={'35px'} alt="logo" /> */}
-                <img src={`/layout/images/logo-remove.png`} className="w-[100px]" alt="logo" />
+                <img src={`/layout/images/logo-remove.png`} className="w-[90px] sm:w-[100px]" alt="logo" />
                 <h3 className="hidden sm:block text-[18px] md:text-[30px]">Цифровой кампус ОшГУ</h3>
             </Link>
 
