149f8c256d26c1586209921bcff83a7d61b34b4f
BekzhanKutmanov <kutmanov@gmail.com>
2025-06-12 14:56:53 +0600

    merge course-add

--- app/(main)/course/page.tsx
+++ app/(main)/course/page.tsx
@@ -126,14 +126,18 @@ export default function Course() {
         const image = product.image;
 
         if (typeof image === 'string') {
-            return <div className='flex justify-center'>
-                <img src={image} alt="Course image" className="w-4rem shadow-2 border-round" />
-            </div>
+            return (
+                <div className="flex justify-center">
+                    <img src={image} alt="Course image" className="w-4rem shadow-2 border-round" />
+                </div>
+            );
         }
 
-        return <div className='flex justify-center'>
-            <img src={'/layout/images/no-image.jpg'} alt="Course image" className="w-4rem shadow-2 border-round" />
-        </div>
+        return (
+            <div className="flex justify-center">
+                <img src={'/layout/images/no-image.jpg'} alt="Course image" className="w-4rem shadow-2 border-round" />
+            </div>
+        );
     };
 
     const getConfirmOptions = (id) => ({
@@ -142,8 +146,8 @@ export default function Course() {
         icon: 'pi pi-info-circle',
         defaultFocus: 'reject',
         acceptClassName: 'p-button-danger',
-        acceptLabel: 'Кийинки кадам',     // кастомная надпись для "Yes"
-        rejectLabel: 'Артка', 
+        acceptLabel: 'Кийинки кадам', // кастомная надпись для "Yes"
+        rejectLabel: 'Артка',
         accept: () => handleDeleteCourse(id),
         reject: () => console.log('Удаление отменено')
     });
@@ -153,7 +157,7 @@ export default function Course() {
         setTimeout(() => {
             setSkeleton(false);
         }, 1000);
-    }
+    };
 
     useEffect(() => {
         handleFetchCourses();
@@ -165,76 +169,76 @@ export default function Course() {
 
     return (
         <div>
-                <FormModal title={editMode ? 'Курсту жаңылоо' : 'Кошуу'} fetchValue={editMode ? handleUpdateCourse : handleAddCourse} clearValues={clearValues} visible={formVisible} setVisible={setFormVisible} start={forStart}>
-                    <div className="flex flex-col gap-1">
-                        <div className="flex flex-col lg:flex-row gap-1 justify-around items-center">
-                            <div className="flex flex-col gap-1 items-center justify-center">
-                                <label className="block text-900 font-medium text-[16px] md:text-xl mb-1 md:mb-2">Аталышы</label>
-                                <InputText
-                                    value={courseTitle}
-                                    // classname?
-                                    placeholder="Аталышы талап кылынат"
-                                    onChange={(e) => {
-                                        setCourseTitle(e.target.value);
-                                        setCourseValue((prev) => ({
-                                            ...prev,
-                                            title: e.target.value
-                                        }));
-                                    }}
-                                />
-                            </div>
-                            <div className="flex flex-col gap-1 items-center justify-center">
-                                <label className="block text-900 font-medium text-[16px] md:text-xl mb-1 md:mb-2">Видео шилтеме</label>
-                                <InputText
-                                    value={video_url}
-                                    placeholder="https://..."
-                                    title="Шилтеме https:// менен башталышы шарт!"
-                                    onChange={(e) => {
-                                        setVideo_url(e.target.value);
-                                        setCourseValue((prev) => ({
-                                            ...prev,
-                                            video_url: e.target.value
-                                        }));
-                                    }}
-                                />
-                            </div>
+            <FormModal title={editMode ? 'Курсту жаңылоо' : 'Кошуу'} fetchValue={editMode ? handleUpdateCourse : handleAddCourse} clearValues={clearValues} visible={formVisible} setVisible={setFormVisible} start={forStart}>
+                <div className="flex flex-col gap-1">
+                    <div className="flex flex-col lg:flex-row gap-1 justify-around items-center">
+                        <div className="flex flex-col gap-1 items-center justify-center">
+                            <label className="block text-900 font-medium text-[16px] md:text-xl mb-1 md:mb-2">Аталышы</label>
+                            <InputText
+                                value={courseTitle}
+                                // classname?
+                                placeholder="Аталышы талап кылынат"
+                                onChange={(e) => {
+                                    setCourseTitle(e.target.value);
+                                    setCourseValue((prev) => ({
+                                        ...prev,
+                                        title: e.target.value
+                                    }));
+                                }}
+                            />
                         </div>
-
-                        <div className="flex flex-col lg:flex-row gap-1 justify-around items-center">
-                            <div className="flex flex-col gap-1 items-center justify-center">
-                                <label className="block text-900 font-medium text-[16px] md:text-xl mb-1 md:mb-2">Мазмуну</label>
-                                <InputTextarea
-                                    autoResize
-                                    value={description}
-                                    rows={5}
-                                    cols={30}
-                                    onChange={(e) => {
-                                        setDesciption(e.target.value);
-                                        setCourseValue((prev) => ({
-                                            ...prev,
-                                            description: e.target.value
-                                        }));
-                                    }}
-                                />
-                            </div>
-
-                            <div className="flex flex-col pag-1 items-center justify-center">
-                                <label className="block text-900 font-medium text-[16px] md:text-xl mb-1 md:mb-2">Сүрөт кошуу</label>
-                                <FileUpload mode="basic" customUpload name="demo[]" accept="image/*" maxFileSize={1000000} onSelect={onSelect} />
-                                {courseValue.image ? (
-                                    <div className="mt-2 text-sm text-gray-700">
-                                        Сүрөт: <b className="text-[12px]">{courseValue.image.name || courseValue.image}</b>
-                                    </div>
-                                ) : (
-                                    <b className="text-[12px] text-red-500">jpeg, png, jpg</b>
-                                )}
-                            </div>
+                        <div className="flex flex-col gap-1 items-center justify-center">
+                            <label className="block text-900 font-medium text-[16px] md:text-xl mb-1 md:mb-2">Видео шилтеме</label>
+                            <InputText
+                                value={video_url}
+                                placeholder="https://..."
+                                title="Шилтеме https:// менен башталышы шарт!"
+                                onChange={(e) => {
+                                    setVideo_url(e.target.value);
+                                    setCourseValue((prev) => ({
+                                        ...prev,
+                                        video_url: e.target.value
+                                    }));
+                                }}
+                            />
                         </div>
                     </div>
-                </FormModal>
-            <div className='flex justify-between items-center my-4 py-2 shadow-[0_2px_1px_0px_rgba(0,0,0,0.1)]'>
+
+                    <div className="flex flex-col lg:flex-row gap-1 justify-around items-center">
+                        <div className="flex flex-col gap-1 items-center justify-center">
+                            <label className="block text-900 font-medium text-[16px] md:text-xl mb-1 md:mb-2">Мазмуну</label>
+                            <InputTextarea
+                                autoResize
+                                value={description}
+                                rows={5}
+                                cols={30}
+                                onChange={(e) => {
+                                    setDesciption(e.target.value);
+                                    setCourseValue((prev) => ({
+                                        ...prev,
+                                        description: e.target.value
+                                    }));
+                                }}
+                            />
+                        </div>
+
+                        <div className="flex flex-col pag-1 items-center justify-center">
+                            <label className="block text-900 font-medium text-[16px] md:text-xl mb-1 md:mb-2">Сүрөт кошуу</label>
+                            <FileUpload mode="basic" customUpload name="demo[]" accept="image/*" maxFileSize={1000000} onSelect={onSelect} />
+                            {courseValue.image ? (
+                                <div className="mt-2 text-sm text-gray-700">
+                                    Сүрөт: <b className="text-[12px]">{courseValue.image.name || courseValue.image}</b>
+                                </div>
+                            ) : (
+                                <b className="text-[12px] text-red-500">jpeg, png, jpg</b>
+                            )}
+                        </div>
+                    </div>
+                </div>
+            </FormModal>
+            <div className="flex justify-between items-center my-4 py-2 shadow-[0_2px_1px_0px_rgba(0,0,0,0.1)]">
                 <div>
-                    <h3 className='text-[36px] m-0'>Курстар</h3>
+                    <h3 className="text-[36px] m-0">Курстар</h3>
                 </div>
 
                 <Button
@@ -249,46 +253,46 @@ export default function Course() {
             </div>
 
             <div className="py-4">
-                { skeleton ? <GroupSkeleton count={courses.length} size={{width:'100%', height:'4rem'}}/>
-                : <DataTable value={courses} breakpoint="960px" responsiveLayout="stack" className="my-custom-table">
-                    <Column field="id" header="Номер" sortable style={{ width: '30px', textAlign: 'center' }}></Column>
-                    <Column field="title" header="Аталышы" className='w-2/3' sortable body={(rowData)=> (
-                        <Link href={'/'}>{rowData.title}</Link>
-                    )}></Column>
-                    <Column header="" style={{ width:'80px' }} body={imageBodyTemplate}></Column>
-                    
-                    <Column
-                        header=""
-                        className="flex justify-center"
-                        body={(rowData) => (
-                            <div className="flex gap-2">
-                                <Button
-                                    icon="pi pi-pencil"
-                                    className="p-button-rounded bg-blue-400"
-                                    onClick={() => {
-                                        setEditMode(true);
-                                        setSelectedCourse(rowData);
-                                        setCourseValue({
-                                            title: rowData.title || '',
-                                            description: rowData.description || '',
-                                            video_url: rowData.video_url || '',
-                                            image: rowData.image || ''
-                                        });
-                                        setCourseTitle(rowData.title);
-                                        setVideo_url(rowData.video_url);
-                                        setDesciption(rowData.description);
-                                        setFormVisible(true);
-                                    }}
-                                />
-                                <ConfirmModal confirmVisible={getConfirmOptions(rowData.id) } />
-                                <Button className=" bg-blue-400" icon="pi pi-arrow-right">
-                                    <Link href={'#'}></Link>
-                                </Button>
-                            </div>
-                        )}
-                    />
-                </DataTable>
-                }
+                {skeleton ? (
+                    <GroupSkeleton count={courses.length} size={{ width: '100%', height: '4rem' }} />
+                ) : (
+                    <DataTable value={courses} breakpoint="960px" responsiveLayout="stack" className="my-custom-table">
+                        <Column field="id" header="Номер" sortable style={{ width: '30px', textAlign: 'center' }}></Column>
+                        <Column field="title" header="Аталышы" className="w-2/3" sortable body={(rowData) => <Link href={'/'}>{rowData.title}</Link>}></Column>
+                        <Column header="" style={{ width: '80px' }} body={imageBodyTemplate}></Column>
+
+                        <Column
+                            header=""
+                            className="flex justify-center"
+                            body={(rowData) => (
+                                <div className="flex gap-2">
+                                    <Button
+                                        icon="pi pi-pencil"
+                                        className="p-button-rounded bg-blue-400"
+                                        onClick={() => {
+                                            setEditMode(true);
+                                            setSelectedCourse(rowData);
+                                            setCourseValue({
+                                                title: rowData.title || '',
+                                                description: rowData.description || '',
+                                                video_url: rowData.video_url || '',
+                                                image: rowData.image || ''
+                                            });
+                                            setCourseTitle(rowData.title);
+                                            setVideo_url(rowData.video_url);
+                                            setDesciption(rowData.description);
+                                            setFormVisible(true);
+                                        }}
+                                    />
+                                    <ConfirmModal confirmVisible={getConfirmOptions(rowData.id)} />
+                                    <Button className=" bg-blue-400" icon="pi pi-arrow-right">
+                                        <Link href={'#'}></Link>
+                                    </Button>
+                                </div>
+                            )}
+                        />
+                    </DataTable>
+                )}
             </div>
         </div>
     );
