a263710d3bf15e4d689b669e7cf64b443ab8f561
BekzhanKutmanov <kutmanov@gmail.com>
2025-08-28 17:13:12 +0600

    Тестовый 2

--- app/components/BaseLayout.tsx
+++ app/components/BaseLayout.tsx
@@ -6,20 +6,21 @@ import AppFooter from '@/layout/AppFooter';
 import { useEffect } from 'react';
 
 export default function BaseLayout() {
-    useEffect(() => {
-        window.location.href = 'http://oldmooc.oshsu.kg/'; // твоя ссылка
-    }, []);
+    // useEffect(() => {
+    //     window.location.href = 'http://oldmooc.oshsu.kg/'; // твоя ссылка
+    // }, []);
 
-    return null; // ничего не показываем
-    // return (
-    //   <>
-    //     <div className="max-w-[1440px] m-auto flex flex-col min-h-screen">
-    //       <AppTopbar/>
-    //       <main className="flex-1 w-full flex justify-center">
-    //         <HomeClient/>
-    //       </main>
-    //       <AppFooter/>
-    //     </div>
-    //   </>
-    // );
+    // return null; // ничего не показываем
+
+    return (
+      <>
+        <div className="max-w-[1440px] m-auto flex flex-col min-h-screen">
+          <AppTopbar/>
+          <main className="flex-1 w-full flex justify-center">
+            <HomeClient/>
+          </main>
+          <AppFooter/>
+        </div>
+      </>
+    );
 }
