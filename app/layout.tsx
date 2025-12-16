69ea2a836026ead8e24aaa774999e47890f892f7
BekzhanKutmanov <kutmanov@gmail.com>
2025-12-16 09:44:31 +0600

    html+script в layout для глобальной загрузки

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,15 +1,15 @@
 'use client';
 import { LayoutProvider } from '../layout/context/layoutcontext';
 import { PrimeReactProvider } from 'primereact/api';
-import { config } from "@fortawesome/fontawesome-svg-core";
-import "@fortawesome/fontawesome-svg-core/styles.css"; // Импорт стилей
+import { config } from '@fortawesome/fontawesome-svg-core';
+import '@fortawesome/fontawesome-svg-core/styles.css'; // Импорт стилей
 config.autoAddCss = false;
 import 'primereact/resources/primereact.css';
 import 'primeicons/primeicons.css';
 import '../styles/layout/layout.scss';
 import '../styles/demo/Demos.scss';
 
-import '../styles/layout/openCourse.css'
+import '../styles/layout/openCourse.css';
 import './globals.css';
 
 interface RootLayoutProps {
@@ -23,9 +23,33 @@ export default function RootLayout({ children }: RootLayoutProps) {
                 <link id="theme-css" href={`/themes/lara-light-indigo/theme.css`} rel="stylesheet"></link>
             </head>
             <body>
+                <div id="preloader">
+                    <div id="preloader-area">
+                        <div className="spinner"></div>
+                        <div className="spinner"></div>
+                        <div className="spinner"></div>
+                        <div className="spinner"></div>
+                        <div className="spinner"></div>
+                        <div className="spinner"></div>
+                    </div>
+                    <div className="preloader-section preloader-left"></div>
+                    <div className="preloader-section preloader-right"></div>
+                </div>
+
                 <PrimeReactProvider>
                     <LayoutProvider>{children}</LayoutProvider>
                 </PrimeReactProvider>
+
+                {/* Скрипт скрытия */}
+                <script
+                    dangerouslySetInnerHTML={{
+                        __html: `
+                    window.addEventListener('load', () => {
+                    document.documentElement.classList.add('loaded');
+                    });
+                  `
+                    }}
+                />
             </body>
         </html>
     );
