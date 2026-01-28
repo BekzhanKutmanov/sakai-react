9cad7f870974bf29c9da750274558746f40d0e32
BekzhanKutmanov <kutmanov@gmail.com>
2026-01-28 12:24:42 +0600

    сортировка сообщений по типам

--- app/components/notification/Notification.tsx
+++ app/components/notification/Notification.tsx
@@ -1,11 +1,12 @@
 'uce slient';
 
-import { LayoutContext } from "@/layout/context/layoutcontext";
-import { mainNotification } from "@/types/mainNotification";
-import Link from "next/link";
-import { useContext, useEffect, useState } from "react";
-import MyDateTime from "../MyDateTime";
-import { OptionsType } from "@/types/OptionsType";
+import { LayoutContext } from '@/layout/context/layoutcontext';
+import { mainNotification } from '@/types/mainNotification';
+import Link from 'next/link';
+import { useContext, useEffect, useState } from 'react';
+import MyDateTime from '../MyDateTime';
+import { OptionsType } from '@/types/OptionsType';
+import { it } from 'node:test';
 
 export default function Notification({ notification }: { notification: mainNotification[] }) {
     // types
@@ -23,6 +24,11 @@ export default function Notification({ notification }: { notification: mainNotif
 
     const { user, setContextNotificationId } = useContext(LayoutContext);
 
+    const [mainNotification, setMainNotification] = useState<mainNotification[]>([]);
+    const [groupNotificationVisible, setGroupNotificationVisible] = useState(false);
+    const [groupNotifications, setGroupNotifications] = useState<NotificationGroupUi[]>([]);
+    const [groupingFl, setGroupingFl] = useState(false);
+
     const options: OptionsType = {
         year: '2-digit',
         month: 'short', // 'long', 'short', 'numeric'
@@ -32,22 +38,34 @@ export default function Notification({ notification }: { notification: mainNotif
         hour12: false // 24-часовой формат
     };
 
-    // const notificationTypeGrouping = (type: string) => {
-    //     setGroupNotificationVisible(true);
-    //     const forGroup = testNotification.filter((item) => item.type.type === type);
-    //     setGroupNotifications(forGroup);
-    // };
+    const notificationTypeGrouping = (type: string) => {
+        setGroupNotificationVisible(false);
+        const forGroup = notification.filter((item) => item.type.type === type);
+        return forGroup;
+    };
+
+    useEffect(() => {
+        setMainNotification(notification);
+    }, [notification]);
 
     // Получаем массив разных типов
-    const typeArr: NotificationGroupUi[] = [];
-    if (notification?.length > 0) {
-        for (let i = 0; i < notification.length; i++) {
-            const checkType = typeArr.some((item) => item.type.type === notification[i].type.type);
-            if (!checkType) {
-                typeArr.push(notification[i]);
+    useEffect(() => {
+        const typeArr: NotificationGroupUi[] = [];
+        if (notification?.length > 0) {
+            for (let i = 0; i < notification.length; i++) {
+                const checkType = typeArr.some((item) => item.type.type === notification[i].type.type);
+                if (!checkType) {
+                    typeArr.push(notification[i]);
+                }
+            }
+            if (typeArr.length > 0) {
+                if (typeArr.length > 1) {
+                    setGroupNotificationVisible(true);
+                }
+                setGroupNotifications(typeArr);
             }
         }
-    }
+    }, []);
 
     const stop = (e: any) => {
         e.stopPropagation();
@@ -58,81 +76,79 @@ export default function Notification({ notification }: { notification: mainNotif
         let findPathResult = '';
         if (user?.is_working) {
             if (item?.type?.type === 'practical') {
-                findPathResult = `/students/${item?.meta?.course_id}/${item?.meta?.connect_id}/${item?.meta?.stream_id}/${item?.meta?.student_id}/${item?.from_user?.id}/${item?.meta?.lesson_id}/${item?.meta?.step_id}`
+                findPathResult = `/students/${item?.meta?.course_id}/${item?.meta?.connect_id}/${item?.meta?.stream_id}/${item?.meta?.student_id}/${item?.from_user?.id}/${item?.meta?.lesson_id}/${item?.meta?.step_id}`;
             } else if (item?.type?.type === 'view') {
-                findPathResult = `/notifications`
+                findPathResult = `/notifications`;
             }
         } else if (user?.is_student) {
             if (item?.type?.type === 'practical') {
-                findPathResult = `/teaching/lessonView/${item?.meta?.lesson_id}/${item?.meta?.id_curricula}/${item?.meta?.stream_id}/${item?.meta?.step_id}`
+                findPathResult = `/teaching/lessonView/${item?.meta?.lesson_id}/${item?.meta?.id_curricula}/${item?.meta?.stream_id}/${item?.meta?.step_id}`;
             }
         }
         return findPathResult;
-    }
+    };
+
+    const groupingSection = (item: NotificationGroupUi) => {
+        return (
+            <div className="flex gap-1 items-center p-1">
+                <b
+                    className="cursor-pointer text-[var(--mainColor)] hover:underline"
+                    onMouseDown={stop}
+                    onClick={(e) => {
+                        stop(e);
+                        const forNotification = notificationTypeGrouping(item?.type?.type);
+                        setMainNotification(forNotification);
+                        setGroupingFl(true);
+                    }}
+                >
+                    {item?.type?.title}
+                </b>
+            </div>
+        );
+    };
 
     // console.log('Массив типов ', typeArr);
     return (
         <div className={`flex flex-col justify-center p-2 gap-1`}>
-            {/* {groupNotificationVisible ? <i className="cursor-pointer pi pi-times flex justify-end" onClick={() => setGroupNotificationVisible(false)}></i> : ''} */}
-            {/* <span onClick={stop} onMouseDown={stop}>stop</span> */}
+            {groupNotificationVisible ? (
+                <div className="flex items-center gap-1 justify-between p-2">
+                    <h3 className="m-0 font-bold text-[17px]">Список сообщений </h3>
+                    <i
+                        className="cursor-pointer pi pi-times flex justify-end"
+                        style={{fontSize: '12px'}}
+                        onClick={(e) => {
+                            stop(e);
+                            setGroupNotificationVisible(false);
+                        }}
+                    ></i>
+                </div>
+            ) : (
+                ''
+            )}
 
-            {/* Отображаем уже сгруппированные например только практические  */}
-            {/* {groupNotificationVisible && groupNotifications?.length > 0
-                    ? groupNotifications?.map((item) => {
-                          let path = '';
-                          if (user?.is_working && item?.type?.type === 'practical') {
-                              path = `/students/${item?.meta?.course_id}/${item?.meta?.connect_id}/${item?.meta?.stream_id}/${item?.meta?.student_id}/${item?.from_user?.id}/${item?.meta?.lesson_id}/${item?.meta?.step_id}`;
-                          } else if (user?.is_student && item?.type?.type === 'practical') {
-                              path = `/teaching/lessonView/${item?.meta?.lesson_id}/${item?.meta?.id_curricula}/${item?.meta?.stream_id}/${item?.meta?.step_id}`;
-                          }
+            {groupingFl && (
+                <div
+                    className="cursor-pointer flex items-center justify-end gap-1 text-sm"
+                    onClick={(e) => {
+                        stop(e);
+                        setMainNotification(notification);
+                        setGroupingFl(false);
+                    }}
+                >
+                    <i className="pi pi-times flex justify-end" style={{fontSize: '12px'}}></i>
+                    <span>Все</span>
+                </div>
+            )}
 
-                          return (
-                              <div key={item?.id} className={`w-full flex flex-col justify-center shadow p-2 gap-1 sm:gap-2`}>
-                                  <div className="w-full flex justify-between">
-                                      <Link onClick={() => setContextNotificationId(item?.id)} className="cursor-pointer hover:underline" href={path}>
-                                          <b className="text-[var(--mainColor)] text-[12px] sm:text-[14px]">{item?.type?.title}</b>
-                                      </Link>
-                                      <span className="text-sm w-[11px] h-[11px] sm:w-[13px] sm:h-[13px] rounded-full bg-[var(--amberColor)]"></span>
-                                  </div>
-
-                                  {user?.is_student && item?.type?.type === 'practical' && (
-                                      <b className="text-[13px] max-w-[350px] text-nowrap overflow-hidden text-ellipsis" title={item?.title}>
-                                          {item?.title}
-                                      </b>
-                                  )}
-                                  <p className="m-0 text-[11px] sm:text-[12px]">
-                                      {item?.from_user?.last_name} {item?.from_user?.name}
-                                  </p>
-                                  <div className="w-full relative flex">
-                                      <p className="absolute right-0 -top-1 sm:-top-3 text-[9px] sm:text-[10px] m-0">
-                                          <MyDateTime createdAt={item?.created_at} options={options} />
-                                      </p>
-                                  </div>
-                              </div>
-                          );
-                      })
-                    : ''} */}
-
-            {/* {!groupNotificationVisible ? (
-                typeArr?.length > 2 ? (
-                    typeArr.map((el: any) => {
-                        return (
-                            <div className="flex gap-1 items-center">
-                                <span>Список сообщений </span>
-                                <b className="cursor-pointer text-[var(--mainColor)] hover:underline" onClick={() => notificationTypeGrouping(el.type.type)}>
-                                    {el.type.type}
-                                </b>
-                            </div>
-                        );
-                    })
-                ) : */}
-            {notification?.length > 0 ? (
-                notification?.map((item) => {
+            {groupNotificationVisible ? (
+                groupNotifications?.map((item) => groupingSection(item))
+            ) : mainNotification?.length > 0 ? (
+                mainNotification?.map((item) => {
                     const path = findPath(item);
                     return (
                         <div key={item?.id} className={`w-full flex flex-col justify-center shadow p-2 gap-1 sm:gap-2`}>
                             <div className="w-full flex justify-between">
-                                <Link onClick={() => setContextNotificationId(item?.id)} className="cursor-pointer hover:underline" href={path}>
+                                <Link onClick={() => setContextNotificationId(item?.id)} className="cursor-pointer underline" href={path}>
                                     <b className="text-[var(--mainColor)] text-[12px] sm:text-[14px]">{item?.type?.title}</b>
                                 </Link>
                                 <span className="text-sm w-[11px] h-[11px] sm:w-[13px] sm:h-[13px] rounded-full bg-[var(--amberColor)]"></span>
@@ -160,4 +176,4 @@ export default function Notification({ notification }: { notification: mainNotif
             )}
         </div>
     );
-};
+}
