a597c02d03227eb676f03f7f276ab24c26f902c9
BekzhanKutmanov <kutmanov@gmail.com>
2026-02-24 11:49:18 +0600

    Ðмиддлвар

--- middleware.tsx
+++ middleware.tsx
@@ -1,8 +1,29 @@
-import { NextResponse } from 'next/server'
-import type { NextRequest } from 'next/server'
+import { NextResponse } from "next/server";
+import type { NextRequest } from "next/server";
 
 export function middleware(request: NextRequest) {
-    console.log(request.cookies.getAll())
+    const { pathname, search } = request.nextUrl;
 
-    return NextResponse.next()
+    // полный путь куда хотел пользователь
+    const redirectTo = pathname + search;
+
+    // берём именно access_token
+    const accessToken = request.cookies.get("access_token")?.value;
+
+    // если нет токена → редирект на логин
+    if (!accessToken) {
+        const loginUrl = new URL("/auth/login", request.url);
+
+        loginUrl.searchParams.set("redirect", redirectTo);
+
+        console.log("🔒 No token → redirect:", redirectTo);
+
+        return NextResponse.redirect(loginUrl);
+    }
+
+    return NextResponse.next();
 }
+
+export const config = {
+    matcher: ["/students/:path*", "/teaching/lessonView/:path*"],
+};
